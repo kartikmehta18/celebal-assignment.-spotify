8fe277f4c31cf95d8bf8b953ffc6e9bd8f076c00
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-07-08 00:00:35 +0530

    api handeling

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,48 @@
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// paste code from RapidAPI
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'x-rapidapi-key': 'f8ec8c31c7msh795d3cfa1e61a8bp1ab889jsn2b704c1a14d6',
+//     'x-rapidapi-host': 'shazam-core.p.rapidapi.com'
+//   }
+// };
+
+// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world?country_code=IN', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+
+
+export const shazamCoreApi = createApi({
+    // an api must have a reducerPath - simply the name of our API
+    reducerPath: 'shazamCoreApi',
+    // now we can call in inside our store.js
+
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+
+        // prepare headers before each API call
+        // so we won't have to provide "options" everytime
+        prepareHeaders: (headers) => {
+            headers.set('x-rapidapi-key', 'f8ec8c31c7msh795d3cfa1e61a8bp1ab889jsn2b704c1a14d6')  // now we no longer need "options" & "fetch function" (which we've commented above)
+            return headers;
+
+        },
+    }),
+
+    endpoints: (builder) => ({
+        // building all of the endpoints of the API we are gonna call
+
+        getTopCharts: builder.query({ query: () => '/charts/world?country_code=IN' }),
+    })
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
+
+// now use useGetTopChartsQuery in Discover.jsx
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import playerReducer from './features/playerSlice';
 
+import { shazamCoreApi } from './services/shazamCore';
+
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
+    // Add the shazamCoreApi reducer to the store
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
